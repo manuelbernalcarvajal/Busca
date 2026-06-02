b92c2a0945c44e0ff1f243273a9827ad959a85a4
manuelbernalcarvajal <119059748+manuelbernalcarvajal@users.noreply.github.com>
2026-06-02 16:41:07 +0200

    Delete #!/bin/entrypoint.sh

--- #!/bin/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Iniciando el crawler ético de datos gubernamentales..."
-
-# Aquí es donde le diremos a Scrapy que arranque a trabajar
-# 'mi_spider' será el nombre del script que raspee el BOE.
-scrapy crawl mi_spider
